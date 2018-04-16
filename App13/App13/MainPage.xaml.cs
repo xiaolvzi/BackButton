410c5a7e74701028946d255439e2bf3a16b1a225
Joe Lv <joel@wicresoft.com>
2018-04-16 15:06:43 +0800

    first commit

---
+++ App13/App13/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App13
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            bt.Clicked += Bt_Clicked;
+		}
+
+        private void Bt_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+           
+        }
+    }
+}
